6890d9d58cf09cc8706ab2afaf0869acd5b24c4b
mlhollestelle <martijnhollestelle@gmail.com>
2021-04-13 21:50:51 +0300

    regenerate webpage

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
-bike_roads_map: generate_map.R data/interim/digiroad_merged.gpkg
+bike_roads_map: generate_map.R data/interim/digiroad_merged.gpkg create_lines
 	rscript generate_map.R
 
 create_lines: merge_digiroad.sh simplify_roads.sh data/raw/digiroad_*.gpkg fetch_data
 	sh merge_digiroad.sh
+	sh simplify_roads.sh
 
 fetch_data: fetch_digiroad.sh
 	sh fetch_digiroad.sh
--- generate_map.R
+++ generate_map.R
@@ -4,9 +4,21 @@ library(leaflet)
 library(htmlwidgets)
 
 roads = read.gpkg("data/processed/digiroad_simple.gpkg")
-lines = SpatialLines(roads@lines)
+roads = sp.to.lines(roads)
 
-widget = leaflet() %>% addProviderTiles("CartoDB.Positron") %>%
+
+
+lines = SpatialLines(roads$lines)
+
+# widget = 
+  
+leaflet() %>% addProviderTiles("CartoDB.Positron") %>%
   addPolylines(data=lines, weight = 3, opacity = 1, color = "#420d91")
 
 saveWidget(widget, file="index.html")
+
+
+
+
+leaflet() %>% addProviderTiles("CartoDB.DarkMatter") %>%
+  addPolylines(data=lines, weight = 3, opacity = 0.4, color = "#ffff00")
